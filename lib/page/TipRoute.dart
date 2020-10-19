ddefad0e8857da482b27b512b9b389e8180db8e3
chuxiong <chuxiong@xgd.com>
2020-10-19 16:55:09 +0800

    feat:新增TipRoute WillPopScope返回数据

--- lib/page/TipRoute.dart
+++ lib/page/TipRoute.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+
 ///参数传递练习1
 class TipsRoute extends StatelessWidget {
   static const String routerName = '/TipsRoute';
@@ -9,27 +10,33 @@ class TipsRoute extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(icon: Icon(Icons.arrow_back_ios), onPressed: (){
-          Navigator.of(context).pop('点击 title 中 back icon 返回 ');
-        }),
-        title: Text('TipsRoute'),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(18),
-        child: Center(
-          child: Column(
-            children: <Widget>[
-              Text('receiver text: $_text '),
-              FlatButton(
-                onPressed: () => {
-                  Navigator.of(context).pop('我是返回值')
-                },
-                child: Text('pop TipRoute'),
-                color: Colors.blue,
-              )
-            ],
+    return WillPopScope(
+      onWillPop: () {
+        Navigator.of(context).pop('通过 WillPopScope 方式返回数据');
+        return Future.value(false);
+      },
+      child: Scaffold(
+        appBar: AppBar(
+         /* leading: IconButton(
+              icon: Icon(Icons.arrow_back_ios),
+              onPressed: () {
+                Navigator.of(context).pop('点击 title 中 back icon 返回 ');
+              }),*/
+          title: Text('TipsRoute'),
+        ),
+        body: Padding(
+          padding: EdgeInsets.all(18),
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                Text('receiver text: $_text '),
+                FlatButton(
+                  onPressed: () => {Navigator.of(context).pop('我是返回值')},
+                  child: Text('pop TipRoute'),
+                  color: Colors.blue,
+                )
+              ],
+            ),
           ),
         ),
       ),
